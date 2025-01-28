76212153dc7e5ffdbb7a994da56f84e64446c66f
ItsWip <pswaraj0614@gmail.com>
2025-01-28 17:31:44 +0530

    model added

---
+++ training_model/web.py
@@ -0,0 +1,12 @@
+import os
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title= "Prediction of Disease Outbreak",
+                   layout="wide",
+                   page_icon=":doctor:")
+diabetes_model= pickle.load(open(r"C:\Users\Sheetal Patil\Documents\GitHub\Prediction_Of_Disease_Outbreak\training_model\daibetes\diabetes_model.sav", "rb"))
+heart_disease= pickle.load(open(r"C:\Users\Sheetal Patil\Documents\GitHub\Prediction_Of_Disease_Outbreak\training_model\heart\heart_disease_model.sav", "rb"))
+parkinsons= pickle.load(open(r"C:\Users\Sheetal Patil\Documents\GitHub\Prediction_Of_Disease_Outbreak\training_model\parkinsons\parkinsons_prediction_model", "rb"))
+
