f269026b1ea652086da3dc1a2b1110cf98877745
ItsWip <pswaraj0614@gmail.com>
2025-01-28 17:49:22 +0530

    side bar done

--- training_model/web.py
+++ training_model/web.py
@@ -10,3 +10,8 @@ diabetes_model= pickle.load(open(r"C:\Users\Sheetal Patil\Documents\GitHub\Predi
 heart_disease= pickle.load(open(r"C:\Users\Sheetal Patil\Documents\GitHub\Prediction_Of_Disease_Outbreak\training_model\heart\heart_disease_model.sav", "rb"))
 parkinsons= pickle.load(open(r"C:\Users\Sheetal Patil\Documents\GitHub\Prediction_Of_Disease_Outbreak\training_model\parkinsons\parkinsons_prediction_model", "rb"))
 
+with st.sidebar:
+    selected= option_menu('Prediction of disease outbreak sustem',['Diabetes Prediction','Heart Disease Prediction','Parkinsons Prediction'],
+                          menu_icon="hospital-fill", icons=['activity','heart','person'],
+                          default_index=0)
+    
